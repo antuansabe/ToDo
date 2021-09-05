735038e7f2cf440e4e7d793dc3edc003d5bb2865
Antonio DroCode <antuansabe@gmail.com>
2021-09-05 12:18:40 -0500

    feature(TodoSearch) add useState Hook to handle our state

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React,  { useState} from 'react';
 
 function TodoSearch() {
+
+
+    const [search, setSearch] = useState('')
+    
+    const onSearchValueChange = (event) => {
+        console.log(event.target.value);
+        setSearch(event.target.value);
+    };
+
+
+
     return (
-        <input className="input__counter" placeholder="Buscar Tareas" />
-    );
+    <>    
+        <input 
+        className="input__counter"
+        placeholder="Buscar Tareas"
+        value={search}
+        onChange={onSearchValueChange}
+        />
+        
+        <p>{ search }  </p>
+    
+    </>
+        );
 }
 
 export { TodoSearch } ;
